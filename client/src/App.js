3f732b6e95c8f4e6a6715f2ccce5f02c6115cda4
Ranjit Kairi <ranjitkoiri009@gmail.com>
2022-02-11 00:21:16 +0530

    progtress

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { v4 as uuid } from "uuid";
+// components
+import Editor from "./components/Editor";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate replace to={`/docs/${uuid()}`} />} />
+        <Route path="/docs/:id" element={<Editor />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
